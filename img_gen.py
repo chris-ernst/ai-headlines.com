afa9ace05c5eb3b4a879f9f1258e99397c36f070
LumiyoApp <150249376+LumiyoApp@users.noreply.github.com>
2024-01-31 14:27:30 +0100

    Update 1.5

--- img_gen.py
+++ img_gen.py
@@ -10,64 +10,46 @@ public_key = os.getenv('UPLOADCARE_PUBLIC_KEY')
 secret_key = os.getenv('UPLOADCARE_PRIVATE_KEY')
 
 
-model = replicate.models.get("prompthero/openjourney")
-version = model.versions.get(
-  "9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb")
-
-promptTuning = ", highly detailed, volumetric lighting, octane render, 4k resolution, trending on artstation, masterpiece"
-                # highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration, art by artgerm and greg rutkowski and alphonse mucha, 8k
-
-
+promptTuning = ", realistic photo, associated press picture"
 
 def sdCall(promptSlug):
 
-    prompt = "mdjrny-v4 style " + promptSlug + promptTuning 
+    prompt = promptSlug + promptTuning
     print(prompt)
 
-    # https://replicate.com/prompthero/openjourney/versions/9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb#input
-    inputs = {
-    # Input prompt
-    'prompt': prompt ,
-    
-    # Width of output image. Maximum size is 1024x768 or 768x1024 because
-    # of memory limits
-    'width': 512,
-
-    # Height of output image. Maximum size is 1024x768 or 768x1024 because
-    # of memory limits
-    'height': 512,
-
-    # Number of images to output
-    'num_outputs': 1,
-
-    # Number of denoising steps
-    # Range: 1 to 500
-    'num_inference_steps': 50,
-
-    # Scale for classifier-free guidance
-    # Range: 1 to 20
-    'guidance_scale': 7,
-
-    # Random seed. Leave blank to randomize the seed
-    # 'seed': 13,
-    }
-
-    # https://replicate.com/prompthero/openjourney/versions/9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb#output-schema
-    output = version.predict(**inputs)
-    return(output[0])
+    output = replicate.run(
+        "stability-ai/sdxl:39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b",
+        input={
+            "width": 1024,
+            "height": 1024,
+            'prompt': prompt,
+            "refine": "expert_ensemble_refiner",
+            "scheduler": "K_EULER",
+            "lora_scale": 0.6,
+            "num_outputs": 1,
+            "guidance_scale": 7.5,
+            "apply_watermark": False,
+            "high_noise_frac": 0.8,
+            "negative_prompt": "",
+            "prompt_strength": 0.8,
+            "num_inference_steps": 50
+        }
+    )
+    print(output)
+    return(output)
 
 
 
-
-#from pyuploadcare import Uploadcare, File
 
 def uploadCare(imgAnswer):
-
     uploadcare = Uploadcare(public_key, secret_key)
-    uploadcareFile = uploadcare.upload(imgAnswer)
-    imgLink=str(uploadcareFile)+"out0.png"
-
+    imgLink = uploadcare.upload_from_url_sync(
+        imgAnswer,
+        check_duplicates=True,
+        save_duplicates=False
+    )
     return(imgLink)
+
 
 
 
--- text_gen.py
+++ text_gen.py
@@ -1,29 +1,70 @@
 import os
 from dotenv import load_dotenv
-import openai
+from openai import OpenAI
+import json
 
 load_dotenv()
+client = OpenAI()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
+GPT_MODEL="gpt-4-turbo-preview"
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_headlines",
+            "description": "Invent a fictional future news headline",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "headline": {
+                        "type": "string",
+                        "description": "The headline of the fictional news story",
+                    },
+                    "paragraph": {
+                        "type": "string",
+                        "description": "The intro paragraph of the fictional news story",
+                    },
+                },
+                "required": ["headline", "paragraph"],
+            },
+        }
+    },
+]
 
 
-# Function gptCall takes a text promt as a String and returns headline and body paragraph as a list. 
+
+def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL):
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+            tools=tools,
+            tool_choice=tool_choice,
+        )
+        return response
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception: {e}")
+        return e
+
+
 
 def gptCall(textPrompt):
-    completion = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "user", "content": textPrompt}])
-    
-    gptAnswer = completion.choices[0].message.content
-    gptAnswer = (gptAnswer.splitlines())
-    #print(gptAnswer) 
+    chat_response = chat_completion_request(
+        messages=textPrompt,
+        tools=tools,
+        tool_choice={"type": "function", "function": {"name": "get_headlines"}}
+    )
 
-    while('' in gptAnswer):
-        gptAnswer.remove("")
-    gptAnswer[0] = gptAnswer[0].strip('\"')
-    #print(gptAnswer[0])
+    # print(chat_response.choices[0].message)
+    gptAnswer = json.loads(chat_response.choices[0].message.tool_calls[0].function.arguments)
 
-    gptAnswer[0].replace('Headline: ', '')
-    #print(gptAnswer[0]) ###
+    # headline = gptAnswer["headline"]
+    # print("Headline: " + headline)
+    # paragraph = gptAnswer["paragraph"]
+    # print("Paragraph: " + paragraph)
 
     return(gptAnswer)
-
-gptCall("Write an English news headline and intro paragraph of a event in Kongolese politics in a strange and unlikely fictional future timeline.")
